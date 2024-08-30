dbd4e0900f7d691de5b50b3899988b49a74b1da5
Yenny Durango <yennycarolinadurango@gmail.com>
2024-08-30 12:39:22 -0500

    modificaciones

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const App = () => {
+  return (
+    <main className='overflow-x-hidden bg-white text-dark'>
+      <Navbar/>
+    </main>
+  )
+}
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { IoMdMenu } from "react-icons/io";
+const NavbarMenu = [
+  {
+    id: 1,
+    title: "Home",
+    path: "/",
+  },
+  {
+    id: 2,
+    title: "Services",
+    link: "#",
+  },
+  {
+    id: 3,
+    title: "About Us",
+    link: "#",
+  },
+  {
+    id: 4,
+    title: "Our Team",
+    link: "#",
+  },
+  {
+    id: 5,
+    title: "Contact Us",
+    link: "#",
+  },
+];
+
+const Navbar = () => {
+  return <div>Navbar</div>;
+}
+export default Navbar;
