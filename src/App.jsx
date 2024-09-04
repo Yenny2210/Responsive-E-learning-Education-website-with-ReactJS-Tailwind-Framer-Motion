492470aa9301e8928789a478923b09208092f922
Yenny Durango <yennycarolinadurango@gmail.com>
2024-09-04 12:22:03 -0500

    se agrego el banner y se termino hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
+import Services from "./components/Services/Services";
+import Banner from "./components/Banner/Banner";
 
 const App = () => {
   return (
     <main className="overflow-x-hidden bg-white text-dark">
-      <Navbar />
       <Hero />
+      <Services />
+      <Banner />
     </main>
   )
 }
